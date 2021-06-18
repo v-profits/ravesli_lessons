230718b204cab9327e6f5d04cc7febbbaf000678
profits <v-profit@yandex.ru>
2021-06-18 11:29:26 +0400

    урок 114.2 - выполнен

--- ravesli_lessons/ravesli_lessons.cpp
+++ ravesli_lessons/ravesli_lessons.cpp
@@ -1,27 +1,48 @@
 ﻿#include <iostream>
 
 class Numbers{
-public:
-	int m_1;
-	int m_2;
+    double m_a;
+    double m_b;
+    double m_c;
 
-	void set(int x, int y) {
-		m_1 = x;
-		m_2 = y;
-	}
-	 void print() {
-		std::cout << "Numbers(" << m_1 << ", " << m_2 << ")\n";
-	}
+public:
+    void setValues(double a, double b, double c) {
+        m_a = a;
+        m_b = b;
+        m_c = c;
+    }
+    bool isEqual(const Numbers& point) {
+        return (m_a == point.m_a && m_b == point.m_b && m_c == point.m_c);
+    }
+      void print() {
+        std::cout << "<" << m_a << ", " << m_b << ", " << m_c << ">\n";
+    }
 };
 
 int main()
 {
-	Numbers n1;
-	n1.set(3, 3); // инициализируем объект n1 значениями 3 и 3
+    Numbers point1;
+    point1.setValues(3.0, 4.0, 5.0);
 
-	Numbers n2{ 4, 4 }; // инициализируем объект n2 значениями 4 и 4
-	n1.print();
-	n2.print();
+    Numbers point2;
+    point2.setValues(3.0, 4.0, 5.0);
 
-	return 0;
+    if (point1.isEqual(point2))
+        std::cout << "point1 and point2 are equal\n";
+    else
+        std::cout << "point1 and point2 are not equal\n";
+
+    Numbers point3;
+    point3.setValues(7.0, 8.0, 9.0);
+
+    (point1.isEqual(point3)) ?
+        std::cout << "point1 and point3 are equal\n" :
+        std::cout << "point1 and point3 are not equal\n";
+
+    std::string str = (point1.isEqual(point3)) ?
+        "point1 and point3 are equal\n" :
+        "point1 and point3 are not equal\n";
+    std::cout << str << "\n"; //изменения
+
+    return 0;
 }
