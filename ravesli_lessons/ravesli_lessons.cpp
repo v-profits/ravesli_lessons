77e88e3a31ba749cd00686760a04421d60ae6db6
profits <v-profit@yandex.ru>
2021-06-18 11:30:52 +0400

    урок 114.3 - выполнен

--- ravesli_lessons/ravesli_lessons.cpp
+++ ravesli_lessons/ravesli_lessons.cpp
@@ -1,48 +1,50 @@
 ﻿#include <iostream>
+#include <cassert>
 
-class Numbers{
-    double m_a;
-    double m_b;
-    double m_c;
+class Stack {
+    int array[10];
+    int lenght;
 
 public:
-    void setValues(double a, double b, double c) {
-        m_a = a;
-        m_b = b;
-        m_c = c;
+    void reset() {
+        lenght = 0;
+        for (int i = 0; i < 10; ++i)
+            array[i] = 0;
     }
-    bool isEqual(const Numbers& point) {
-        return (m_a == point.m_a && m_b == point.m_b && m_c == point.m_c);
+    void print() {
+        std::cout << "( ";
+        for (int j = 0; j < lenght; ++j)
+            std::cout << array[j] << " ";
+        std::cout << ")\n";
     }
-      void print() {
-        std::cout << "<" << m_a << ", " << m_b << ", " << m_c << ">\n";
+    void push(const int value) {
+        array[lenght] = value;
+        ++lenght;
+    }
+    void pop() {
+        assert(lenght != 0 && " В стеке нет значений");
+        array[lenght - 1] = 0;
+        --lenght;
     }
 };
 
 int main()
 {
-    Numbers point1;
-    point1.setValues(3.0, 4.0, 5.0);
+    Stack stack;
+    stack.reset();
+    stack.print();
 
-    Numbers point2;
-    point2.setValues(3.0, 4.0, 5.0);
+    stack.push(3);
+    stack.push(7);
+    stack.push(5);
+    stack.print();
 
-    if (point1.isEqual(point2))
-        std::cout << "point1 and point2 are equal\n";
-    else
-        std::cout << "point1 and point2 are not equal\n";
+    stack.pop();
+    stack.print();
 
-    Numbers point3;
-    point3.setValues(7.0, 8.0, 9.0);
-
-    (point1.isEqual(point3)) ?
-        std::cout << "point1 and point3 are equal\n" :
-        std::cout << "point1 and point3 are not equal\n";
-
-    std::string str = (point1.isEqual(point3)) ?
-        "point1 and point3 are equal\n" :
-        "point1 and point3 are not equal\n";
-    std::cout << str << "\n"; //изменения
+    stack.pop();
+    stack.pop();
+    stack.print();
 
     return 0;
 }
