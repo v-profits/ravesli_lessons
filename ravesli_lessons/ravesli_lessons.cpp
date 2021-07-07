396cf4f8fc6033b75280c309485ed97d0a24f00c
profits <v-profit@yandex.ru>
2021-07-07 16:48:42 +0400

    урок 58

--- ravesli_lessons/ravesli_lessons.cpp
+++ ravesli_lessons/ravesli_lessons.cpp
@@ -1,25 +1,16 @@
 ﻿#include <iostream>
 
-int isEven();
+enum Monster {
+    OGRE,
+    GOBLIN,
+    SKELETON,
+    ORC,
+    TROLL
+};
 
 int main()
 {
-    setlocale(0, "");
-
-    std::cout << "Введите целое число: ";
-    int a = isEven();
-    
-    std::cout << "Введите большее целое число: ";
-    int b = isEven();
-
-    if (a > b) {
-        std::cout << "Меняем значения местами" << std::endl;
-        int c = a;
-        a = b;
-        b = c;
-    }
-    std::cout << "Меньшее число: " << a << std::endl;
-    std::cout << "Большее число: " << b;
+    Monster ogre = OGRE;
 
     return 0;
 }
