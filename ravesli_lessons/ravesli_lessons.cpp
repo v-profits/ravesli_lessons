66994752882c39ff2958bda8e37a19d7cb778a06
profits <v-profit@yandex.ru>
2021-07-10 15:35:18 +0400

    урок 65.1

--- ravesli_lessons/ravesli_lessons.cpp
+++ ravesli_lessons/ravesli_lessons.cpp
@@ -1,85 +1,41 @@
 ﻿#include <iostream>
 
-enum class eMonsters {
-    NONE, //отсутствие монстра 
-    ORGE,
-    GOBLIN,
-    SKELETON,
-    ORC,
-    TROLL
-};
+std::string err = "";
 
-struct sMonster {
-    enum class eMonsters type;
-    std::string name;
-    int healht;
-};
-
-void Monsters(struct sMonster& m, const enum class eMonsters e) {
-    switch (e) {
-    case eMonsters::ORGE:
-        m.type = e;
-        m.name = "шпала";
-        m.healht = 170;
-        break;
-    case eMonsters::GOBLIN:
-        m.type = e;
-        m.name = "чучело";
-        m.healht = 50;
-        break;
-    case eMonsters::SKELETON:
-        m.type = e;
-        m.name = "тощий";
-        m.healht = 200;
-        break;
-    case eMonsters::ORC:
-        m.type = e;
-        m.name = "кувалда";
-        m.healht = 100;
-        break;
-    case eMonsters::TROLL:
-        m.type = e;
-        m.name = "дылда";
-        m.healht = 150;
-        break;
-    default:
-        std::cout << "нет такого монстра";
-    }
-}
-
-const std::string printStr(const enum class eMonsters e) {
-    std::string str = "";
-    e == eMonsters::ORGE ? str = "orge" :
-        e == eMonsters::GOBLIN ? str = "goblin" :
-        e == eMonsters::SKELETON ? str = "skeleton" :
-        e == eMonsters::ORC ? str = "orc" :
-        e == eMonsters::TROLL ? str = "troll" : str = "";
-    return str;
-}
-
-void printMonster(const sMonster m) {
-    if(m.healht != 0) std::cout << "монстр " << printStr(m.type) << " - имя  " << m.name << ", осталось жизни : " << m.healht;
+const int calculate(const int a, const int b, const char ch)
+{
+	switch (ch) {
+	case '+': return a + b;
+	case '-': return a - b;
+	case '*': return a * b;
+	case '/': return a / b;
+	case '%': return a % b;
+	default: err = "error"; return 0;
+	}
 }
 
 int main()
 {
-    setlocale(0, "");
+	setlocale(0, "");
 
-    int i;
-    std::cout << "монстр : ogre-1, goblin-2, skeleton-3, orc-4, troll-5 : ";
-    std::cin >> i;
+	int a, b, c;
+	char ch;
+	std::cout << "первое число : ";
+	std::cin >> a;
+	std::cout << "второе число : ";
+	std::cin >> b;
+	std::cout << "знак : ";
+	std::cin >> ch;
 
-    eMonsters e = eMonsters::NONE;
-    if (i >= 1 && i <= 5) {
-        i == 1 ? e = eMonsters::ORGE :
-        i == 2 ? e = eMonsters::GOBLIN :
-        i == 3 ? e = eMonsters::SKELETON :
-        i == 4 ? e = eMonsters::ORC :
-                 e = eMonsters::TROLL;
-    }
-    sMonster m{ eMonsters::NONE, "", 0 };
-    Monsters(m, e); 
-    printMonster(m);
+	c = calculate(a, b, ch);
 
-    return 0;
+	if (err != "error")
+		std::cout << "результат : " << c;
+	else {
+		std::cout << "результат : " << err;
+		err = "";
+	}
+		
+
+	return 0;
 }
