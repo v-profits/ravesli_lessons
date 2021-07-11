90f2485f8df455c97d2a3d7bdef0cf5330614d55
profits <v-profit@yandex.ru>
2021-07-11 13:02:06 +0400

    урок 67.3

--- ravesli_lessons/ravesli_lessons.cpp
+++ ravesli_lessons/ravesli_lessons.cpp
@@ -2,10 +2,21 @@
 
 int main()
 {
-	char ch = 'a';
-	while (ch <= 'z') {
-		std::cout << ch << ' ' << static_cast<int>(ch) << "\n";
-		++ch;
-	}
-	return 0;
+    int i = 5;
+    while (i >= 1) {
+        int k = i;
+        while (k >= 1)
+            std::cout << k-- << " ";
+        std::cout << "\n";
+        --i;
+    }
+    return 0;
 }
+
+/*
+5 4 3 2 1
+4 3 2 1
+3 2 1
+2 1
+1
+*/
