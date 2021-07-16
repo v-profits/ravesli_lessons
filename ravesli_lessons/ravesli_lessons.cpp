576cb56e3f10add8b4ece48ded9c4712f7a2c440
profits <v-profit@yandex.ru>
2021-07-16 16:21:33 +0400

    урок 86

--- ravesli_lessons/ravesli_lessons.cpp
+++ ravesli_lessons/ravesli_lessons.cpp
@@ -1,42 +1,58 @@
-﻿//80.1
+﻿//86
 #include <iostream>
+#include <cstring>
+#include <Windows.h>
 
 int main()
 {
-	short value = 7; // &value = 0012FF60
-	short otherValue = 3; // &otherValue = 0012FF54
-
-	short* ptr = &value;
-
-	std::cout << &value << '\n'; // 0012FF60
-	std::cout << value << '\n'; // 7
-	std::cout << ptr << '\n'; // 0012FF60
-	std::cout << *ptr << '\n'; // 7
-	std::cout << '\n';
-
-	*ptr = 9;
-
-	std::cout << &value << '\n'; // 0012FF60
-	std::cout << value << '\n'; // 9
-	std::cout << ptr << '\n'; // 0012FF60
-	std::cout << *ptr << '\n'; // 9
-	std::cout << '\n';
-
-	ptr = &otherValue;
-
-	std::cout << &otherValue << '\n'; // 0012FF54
-	std::cout << otherValue << '\n'; // 3
-	std::cout << ptr << '\n'; // 0012FF54
-	std::cout << *ptr << '\n'; // 3
-	std::cout << '\n';
-
-	std::cout << sizeof(ptr) << '\n'; // 4
-	std::cout << sizeof(*ptr) << '\n'; // 2
-
+	SetConsoleCP(1251); SetConsoleOutputCP(1251); // для ввода-вывода на кириллице
+	std::cout << "Сколько имен вы хотите ввести : ";
+	int j;
+	std::cin >> j;
+	std::string str;
+	std::string* arr;
+	arr = new std::string[j];
+	for (int i = 0; i < j; ++i) {
+		std::cout << "имя №" << i + 1 << " : ";
+		std::cin >> arr[i];
+	}
+	for (int i = 0; i < j - 1; ++i) {
+		int index = i;
+		for (int k = 1; k < j; ++k)
+			if (arr[i] > arr[k])
+				index = k;
+		std::swap(arr[i], arr[index]);
+	}
+	for (int i = 0; i < j; ++i)
+		std::cout << arr[i] << ' ';
+	delete[] arr;
 	return 0;
 }
 
 /*
-Какие значения мы получим в результате выполнения следующей программы 
-(предположим, что это 32-битное устройство, и тип short занимает 2 байта) :
+Напишите программу, которая:
+   спрашивает у пользователя, сколько имен он хочет ввести;
+   просит пользователя ввести каждое имя;
+   вызывает функцию для сортировки имен в алфавитном порядке (измените код сортировки методом выбора из урока №77);
+   выводит отсортированный список имен.
+
+Подсказки:
+   Используйте динамическое выделение std::string для хранения имен.
+   std::string поддерживает сравнение строк с помощью операторов сравнения < и >.
+
+Пример результата выполнения вашей программы:
+
+How many names would you like to enter? 5
+Enter name #1: Jason
+Enter name #2: Mark
+Enter name #3: Alex
+Enter name #4: Chris
+Enter name #5: John
+
+Here is your sorted list:
+Name #1: Alex
+Name #2: Chris
+Name #3: Jason
+Name #4: John
+Name #5: Mark
 */
