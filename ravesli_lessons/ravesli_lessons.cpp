7eb74e2aacba4b38adfd9080e5c49079342b0df6
profits <v-profit@yandex.ru>
2021-07-16 20:43:41 +0400

    урок 91

--- ravesli_lessons/ravesli_lessons.cpp
+++ ravesli_lessons/ravesli_lessons.cpp
@@ -1,58 +1,39 @@
-﻿//86
+﻿//91
 #include <iostream>
-#include <cstring>
-#include <Windows.h>
 
 int main()
 {
-	SetConsoleCP(1251); SetConsoleOutputCP(1251); // для ввода-вывода на кириллице
-	std::cout << "Сколько имен вы хотите ввести : ";
-	int j;
-	std::cin >> j;
+	setlocale(0, "");
+	std::string arr[]{ "Sasha","Ivan","John","Orlando","Leonardo","Nina","Anton","Molly" };
 	std::string str;
-	std::string* arr;
-	arr = new std::string[j];
-	for (int i = 0; i < j; ++i) {
-		std::cout << "имя №" << i + 1 << " : ";
-		std::cin >> arr[i];
-	}
-	for (int i = 0; i < j - 1; ++i) {
-		int index = i;
-		for (int k = 1; k < j; ++k)
-			if (arr[i] > arr[k])
-				index = k;
-		std::swap(arr[i], arr[index]);
-	}
-	for (int i = 0; i < j; ++i)
-		std::cout << arr[i] << ' ';
-	delete[] arr;
+	std::cout << "Введите имя : ";
+	std::cin >> str;
+
+	bool flag = false;
+	for (auto& s : arr)
+		if (str == s)
+			flag = true;
+
+	if (flag)
+		std::cout << "Такое имя есть в списке";
+	else
+		std::cout << "Такого имени нет в списке";
+		
 	return 0;
 }
 
 /*
-Напишите программу, которая:
-   спрашивает у пользователя, сколько имен он хочет ввести;
-   просит пользователя ввести каждое имя;
-   вызывает функцию для сортировки имен в алфавитном порядке (измените код сортировки методом выбора из урока №77);
-   выводит отсортированный список имен.
+Объявите фиксированный массив со следующими именами: Sasha, Ivan, John, Orlando, Leonardo, Nina, Anton и Molly. 
+Попросите пользователя ввести имя. 
+Используйте цикл foreach для проверки того, не находится ли имя, введенное пользователем, уже в массиве.
 
-Подсказки:
-   Используйте динамическое выделение std::string для хранения имен.
-   std::string поддерживает сравнение строк с помощью операторов сравнения < и >.
+Пример результата выполнения программы:
 
-Пример результата выполнения вашей программы:
+Enter a name: Sasha
+Sasha was found.
 
-How many names would you like to enter? 5
-Enter name #1: Jason
-Enter name #2: Mark
-Enter name #3: Alex
-Enter name #4: Chris
-Enter name #5: John
+Enter a name: Maruna
+Maruna was not found.
 
-Here is your sorted list:
-Name #1: Alex
-Name #2: Chris
-Name #3: Jason
-Name #4: John
-Name #5: Mark
+Подсказка: Используйте std::string в качестве типа массива.
 */
