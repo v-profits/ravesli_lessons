f173015643aefd0f8b1e4f78d94bc788f056701a
profits <v-profit@yandex.ru>
2021-07-20 13:42:58 +0400

    глава 6.5c

--- ravesli_lessons/ravesli_lessons.cpp
+++ ravesli_lessons/ravesli_lessons.cpp
@@ -1,35 +1,30 @@
-﻿//глава 6.5б
+﻿//глава 6.5c
 #include <iostream>
+
+void printArray(int array[], int length) {
+	for (int i = 0; i < length; ++i)
+        std::cout << array[i] << ' ';
+}
 
 int main()
 {
-    int a = 4;
-    int b = 6;
-
-    const int* ptr = &a;
-    std::cout << *ptr;
-    a = 7;
-    std::cout << *ptr;
-    ptr = &b;
-    std::cout << *ptr;
-
+    int array[]{ 8, 6, 4, 2, 0 };
+	int length = sizeof(array) / sizeof(array[0]);
+	printArray(array, length);
     return 0;
 }
 
 /*
+void printArray(int array[]) {
+	for (const int &element : array)
+		std::cout << element << ' ';
+}
+
 int main()
 {
-    int a = 4;
-    int b = 6;
-
-    const int *ptr = &a;
-    std::cout << *ptr;
-    *ptr = 7;
-    std::cout << *ptr;
-    ptr = &b;
-    std::cout << *ptr;
-
-    return 0;
+	int array[] { 8, 6, 4, 2, 0 };
+	printArray(array);
+	return 0;
 }
 
 Что не так с фрагментом кода, и как бы вы их исправили?
