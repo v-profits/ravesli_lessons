1b447e1134fad6c219002582bd2f2b0e41c10797
profits <v-profit@yandex.ru>
2021-07-20 15:08:30 +0400

    глава 6.6c - карточная игра

--- ravesli_lessons/ravesli_lessons.cpp
+++ ravesli_lessons/ravesli_lessons.cpp
@@ -1,4 +1,4 @@
-﻿//глава 6.6b - карточная игра
+﻿//глава 6.6c - карточная игра
 #include <iostream>
 
 enum eCard {
@@ -11,18 +11,45 @@ enum eSort {
 };
 
 struct sCard {
-	eCard card;
-	eSort sort;
+	eCard Card;
+	eSort Sort;
 };
+
+void printCard(const sCard& card) {
+	switch(card.Card) {
+	case K_2  : std::cout << "2"    ; break;
+	case K_3  : std::cout << "3"    ; break;
+	case K_4  : std::cout << "4"    ; break;
+	case K_5  : std::cout << "5"    ; break;
+	case K_6  : std::cout << "6"    ; break;
+	case K_7  : std::cout << "7"    ; break;
+	case K_8  : std::cout << "8"    ; break;
+	case K_9  : std::cout << "9"    ; break;
+	case K_10 : std::cout << "10"   ; break;
+	case VALET: std::cout << "valet"; break;
+	case DAMA : std::cout << "dama" ; break;
+	case KOROL: std::cout << "korol"; break;
+	case TUZ  : std::cout << "tuz"  ; break;
+	}
+	switch (card.Sort) {
+	case TREF  : std::cout << " tref"  ; break;
+	case BUBNY : std::cout << " bubny" ; break;
+	case CHERVY: std::cout << " chervy"; break;
+	case PIKY  : std::cout << " piky"  ; break;
+	}
+}
 
 int main()
 {
-
+	sCard card;
+	card = {K_2, TREF};
+	printCard(card);
 	return 0;
 }
 
 /*
-b) Каждая карта должна быть представлена структурой Card, 
-в которой хранится информация о достоинстве и масти карты (например, 4 бубны, король трефы). 
-Создайте эту структуру.
+c) Создайте функцию printCard(), 
+параметром которой будет константная ссылка типа структуры Card, 
+которая будет выводить значения достоинства и масти определенной карты в виде 2-буквенного кода 
+(например, валет пики будет выводиться как VP).
 */
