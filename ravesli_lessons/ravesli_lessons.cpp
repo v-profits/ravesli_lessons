69a19b50c92d2ec362778e5e03fe3189e6fd50b3
profits <v-profit@yandex.ru>
2021-07-20 19:07:08 +0400

    глава 6.6e - карточная игра

--- ravesli_lessons/ravesli_lessons.cpp
+++ ravesli_lessons/ravesli_lessons.cpp
@@ -1,21 +1,21 @@
-﻿//глава 6.6d - карточная игра
+﻿//глава 6.6e - карточная игра
 #include <iostream>
 #include <array>
 
-enum Grade {
+enum Grade { // значения карт
 	K_2, K_3, K_4, K_5, K_6, K_7, K_8, K_9, K_10,
 	VALET, DAMA, KOROL, TUZ, cMAX = 13
 
 };
-enum Sort {
+enum Sort { // масти карт
 	TREF, BUBNY, CHERVY, PIKY, sMAX = 4
 };
-struct Card {
+struct Card { // значение и масть карты
 	Grade grade;
 	Sort sort;
 };
-void printCard(const std::array<Card, 52>& card, int i) {
-	switch(card[i].grade) {
+void printCard(const Card& card) { // выводим карту в консоль
+	switch(card.grade) {
 	case K_2  : std::cout << "2"    ; break;
 	case K_3  : std::cout << "3"    ; break;
 	case K_4  : std::cout << "4"    ; break;
@@ -30,29 +30,34 @@ void printCard(const std::array<Card, 52>& card, int i) {
 	case KOROL: std::cout << "korol"; break;
 	case TUZ  : std::cout << "tuz"  ; break;
 	}
-	switch (card[i].sort) {
+	switch (card.sort) {
 	case TREF  : std::cout << " tref"  ; break;
 	case BUBNY : std::cout << " bubny" ; break;
 	case CHERVY: std::cout << " chervy"; break;
 	case PIKY  : std::cout << " piky"  ; break;
 	}
 }
+void printArr(const std::array<Card, 52>& arr) { // перебор всех карт для вывода в консоль
+	for (const auto& card : arr) {
+		printCard(card);
+		std::cout << std::endl;
+	}
+}
 int main()
 {
-	std::array<Card,52>arr;
-	for (int i = 0, k = 0; i < cMAX; ++i)
+	std::array<Card,52>arr; // создаем колоду карт
+	for (int i = 0, k = 0; i < cMAX; ++i) // инициализируем каждую карту
 		for (int j = 0; j < sMAX; ++j) {
 			arr[k].grade = static_cast<Grade>(i);
 			arr[k].sort = static_cast<Sort>(j);
 			++k;
 		}
-	printCard(arr, 51);
+	printArr(arr); // выводим колоду карт в консоль
 	return 0; 
 }
 
 /*
-d) Для представления целой колоды карт (52 карты) создайте массив deck 
-(используя std::array) и инициализируйте каждый элемент определенной картой.
-
-Подсказка: Используйте оператор static_cast для конвертации целочисленной переменной в тип перечисления.
+е) Напишите функцию printDeck(), 
+которая в качестве параметра принимает константную ссылку на массив deck и выводит все значения (карты). 
+Используйте цикл foreach.
 */
